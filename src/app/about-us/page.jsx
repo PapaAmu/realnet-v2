4c1b68df37823977bf33e03216571b987e3182f9
Real Lukhele <realthemba@gmail.com>
2025-10-21 06:55:07 +0200

    export const dynamic = 'force-dynamic';

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,6 +1,9 @@
 import { generateSEO } from "@/lib/SEO";
 import AboutUs from "@/components/pages/company/AboutUs";
 
+export const dynamic = 'force-dynamic';
+
+
 export const metadata = generateSEO({
   title: 'About Realnet Web Solutions | South Africa’s Digital Transformation Partner',
   description: 'Learn more about Realnet Web Solutions — a South African digital agency specializing in web development, mobile apps, and business software. We empower brands and organizations nationwide through modern, results-driven digital solutions.',
